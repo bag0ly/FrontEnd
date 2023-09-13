539d1ac24c9421fc7ffddca0e6a87e241a8efac4
balazsforgo98@gmail.com <balazsforgo98@gmail.com>
2023-09-13 19:19:00 +0200

    tabla

--- jsfrontend/script.js
+++ jsfrontend/script.js
@@ -1,6 +1,27 @@
-const numbers = [45 , 5, 6, 3,55]
+const numbers = [45, 4, 9, 16, 25];
 let txt = "";
-numbers.forEach(majfunkciok())
-function majfunkciok(value, index, array){
-    txt+=numbers;
+numbers.forEach(myFunction);
+
+function myFunction(value) {
+  txt += value +"&nbsp;";
 }
+document.getElementById("kiir").innerHTML=`<table class="table table-striped">
+<thead>
+  <tr>
+    <th>A</th>
+    <th>B</th>
+    <th>C</th>
+    <th>D</th>
+    <th>E</th>
+  </tr>
+</thead>
+<tbody>
+  <tr>
+    <td>${numbers[0]}</td>
+    <td>${numbers[1]}</td>
+    <td>${numbers[2]}</td>
+    <td>${numbers[3]}</td>
+    <td>${numbers[4]}</td>
+  </tr>
+</tbody>
+</table>`
