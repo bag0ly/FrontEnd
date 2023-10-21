77f2e5224ff41915b4fa02694c13bcbd75d661cc
bag0ly <balazsforgo98@gmail.com>
2023-10-21 17:31:30 +0200

    resetbutton

--- alakzatok/index.js
+++ alakzatok/index.js
@@ -18,6 +18,24 @@ document.getElementById("shapes").addEventListener("submit", function(event){
     render();
     console.log(action, shapeName);
 })
+document.addEventListener("DOMContentLoaded", function() {
+    var resetButton = document.getElementById("resetButton");
+
+    var form = document.getElementById("shapes");
+
+    resetButton.addEventListener("click", function() {
+        
+        form.reset();
+
+        state = {
+            circle:0,
+            square:0,
+            rectangle:0
+        };
+        render();
+    });
+    
+});
 function render() {
     document.getElementById("sh-circle").innerHTML=state.circle
     document.getElementById("sh-square").innerHTML=state.square
