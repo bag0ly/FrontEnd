1a57fe6ef2b529bb366e1329e70f62d64fd31ee9
bag0ly <balazsforgo98@gmail.com>
2023-10-25 13:12:45 +0200

    react first impressions v2

---
+++ masodik-react-app/src/App.js
@@ -0,0 +1,28 @@
+import React, { useDebugValue, useEffect } from 'react';
+import { useState } from 'react';
+import './App.css';
+
+
+function App() {
+  const [clickCount,setClickCount] = useState(0)
+
+  useEffect(()=>{
+    document.title="Kattintások száma: "+ clickCount;
+    console.log(clickCount);
+  },[clickCount]);
+
+  return (
+    <div className="card w-25">
+      Kattintások száma: {clickCount}
+      <button className='btn btn-primary'
+      onClick={()=>{
+        setClickCount((elozo)=> elozo+1);
+      }}
+      >
+        Kattints!
+      </button>
+    </div>
+  );
+}
+
+export default App;
