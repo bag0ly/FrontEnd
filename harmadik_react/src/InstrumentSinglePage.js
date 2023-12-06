55b2f1002e29a102db5f0a6b5adc603de1d6c912
bag0ly <balazsforgo98@gmail.com>
2023-12-06 11:13:22 +0100

    Routes_in_react

---
+++ harmadik_react/src/InstrumentSinglePage.js
@@ -0,0 +1,38 @@
+import {useEffect, useState} from 'react'
+export function InstrumentSinglePage()
+{
+    const [instruments,setInstruments] = useState([]);
+    const [isFetchPending,setFetchPending] = useState(false);
+
+    useEffect(() => {
+        setFetchPending(true);
+        fetch("https://kodbazis.hu/api/instruments", { credentials: "include" })
+          .then((res) => res.json())
+          .then((hangszerek) => setInstruments(hangszerek))
+          .catch(console.log)  
+          .finally(() => {
+            setFetchPending(false);
+          });
+        }, []);
+    return(
+        <div className='p-5 m-auto text-center content bg-ivory'>
+                {isFetchPending ? (<div className="spinner-border"></div>):(
+                    <div>
+                        <h2>Hangszorok</h2>
+                        {instruments.map((instrument)=>(
+                            <div className='card sol-sm-3 d-inline-block m-1 p-2'>
+                                <h6 className='text-muted'>{instrument.brand}</h6>
+                                <h5 className='text-muted'>{instrument.name}</h5>
+                                <div>{instrument.price}.- HUF</div>
+                                <div className='small'>Készleten: {instrument.quantity} db</div>
+                                <div className='card-body'>
+                                    <img className='img-fluid' style={{maxHeight:200}} alt="hello"
+                                src={instrument.imageURL ? instrument.imageURL : "https://via.placeholder.com/400x800"}/>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                )}
+        </div>
+    )
+} 
