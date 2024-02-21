5f0e62d2579ed258be58d327c2b3444c73f4050b
bag0ly <balazsforgo98@gmail.com>
2024-02-21 12:37:03 +0100

    jwtfeladat

---
+++ jwttoken/src/App.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import './App.css';
+
+function App() {
+  const [token, setToken] = useState('');
+  const [username, setUsername] = useState("user");
+  const [password, setPassword] = useState("password");
+  const [result, setResult] = useState(null);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios.post('https://jwt.sulla.hu/login', { username, password })
+      .then((res) => {
+        const newToken = res.data.token;
+        setToken(newToken);
+
+        if (newToken !== '') {
+          axios.get('https://jwt.sulla.hu/termekek', {
+            headers: { Authorization: 'Bearer ' + newToken },
+          })
+            .then(function (response) {
+              console.log(response);
+              setResult(response.data); 
+            })
+            .catch(function (error) {
+              console.log(error);
+            });
+        }
+      })
+      .catch(function (error) {
+        console.log(error.response.data);
+      });
+  }
+
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:
+          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </label>
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+
+      {/* Display the result */}
+      {result && (
+        <div>
+          <h3>API Result:</h3>
+          <p>{JSON.stringify(result)}</p>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
